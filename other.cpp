0af0f8f85f863772a5a19b25b1804dd887c2e4e8
reddemon <lucas_ortiz_89@yahoo.com>
2022-01-05 18:23:59 -0800

    implemented functions

--- other.cpp
+++ other.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+void sort(int* A, int length);
+void display(int* A, int length);
+
 int main() {
   int digit;
   cout << "Enter the number of iterations you would like" << endl;
@@ -10,19 +13,26 @@ int main() {
   	cout << "Enter an integer: " << endl;
   	cin >> array[i];
   }
-  for(int i = 0; i < digit; i++) {
+  sort(array,digit);
+  cout << "Your sorted inputs are: ";
+  display(array,digit);
+
+	return 0;
+}
+
+void display(int* A, int length) {
+	for(int i = 0; i < length; i++) {
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
+void sort(int* A, int length) {
+	for(int i = 0; i < length; i++) {
   	for(int j = 0; j < i; j++) {
-			if(array[j] > array[i]) {
-				swap(array[j],array[i]);
+			if(A[j] > A[i]) {
+				swap(A[j],A[i]);
 			}
   	}
   }
-  
-  cout << "Your sorted inputs are: ";
-  for(int i = 0; i < digit; i++) {
-  	cout << array[i] << " ";
-  }
-  cout << endl;
-
-	return 0;
 }
