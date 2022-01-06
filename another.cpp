ac84fb966163e056d3fa0c9954fe456ca7c25d2e
reddemon <lucas_ortiz_89@yahoo.com>
2022-01-05 16:00:38 -0800

    next day changes

--- another.cpp
+++ another.cpp
@@ -5,7 +5,7 @@ int main() {
   cout << "This is irritating" << endl;
   int x = 5;
   for(int i = 0; i < x; i++) {
-   cout << "This is iteration " << i + 1 << "." << endl;
+   cout << "This is iteration " << i + 1 << "!" << endl;
   }
   return 0;
 
